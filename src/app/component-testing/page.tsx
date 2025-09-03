81dedc7f90435fe910cd9279c498580082282204
mgrizqy <mgrizqy.work@gmail.com>
2025-09-03 19:40:33 +0700

    feat: implement CheckoutSection & OrderSummaary components UI

--- src/app/component-testing/page.tsx
+++ src/app/component-testing/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { useState } from "react";
 import CartList from "@/components/cart/CartList";
+import CheckoutSection from "@/components/cart/CheckoutSection";
 import { mockCartItems } from "@/components/cart/dummy-data/Data-CartItem";
 import { CartItemProps } from "@/components/types";
-import { useState } from "react";
 
 export default function ComponentTest() {
   const [items, setItems] = useState<CartItemProps[]>(mockCartItems);
@@ -31,13 +32,18 @@ export default function ComponentTest() {
   };
 
   return (
-    <section className="space-y-4">
-      <CartList
-        items={items}
-        onDecrement={handleDecrement}
-        onIncrement={handleIncrement}
-        onRemove={handleRemove}
-      />
+    <section className="grid grid-cols-1 gap-6 md:grid-cols-3">
+      <div className="md:col-span-2 space-y-4">
+        <CartList
+          items={items}
+          onDecrement={handleDecrement}
+          onIncrement={handleIncrement}
+          onRemove={handleRemove}
+        />
+      </div>
+      <div>
+        <CheckoutSection items={items} />
+      </div>
     </section>
   );
 }
