bfba6427dc06f8e8bc37c1934e739586c81b5f59
portoarco <arcoanggoro78@gmail.com>
2025-09-18 22:58:53 +0700

    fix conflict-3

--- next.config.ts
+++ next.config.ts
@@ -27,17 +27,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-<<<<<<< HEAD
-        hostname: "lh3.googleusercontent.com"
-      }
-=======
         hostname: "loremflickr.com",
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
       },
->>>>>>> 2c88d750f7648d0c21d67537157b4d03496a157d
     ],
   },
 };
