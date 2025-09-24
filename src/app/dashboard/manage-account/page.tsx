f39bec3282effbaa88cf6508b4cf4867fa7f07bc
portoarco <arcoanggoro78@gmail.com>
2025-09-24 14:00:12 +0700

    Refactor

--- src/app/dashboard/manage-account/page.tsx
+++ src/app/dashboard/manage-account/page.tsx
@@ -8,12 +8,12 @@ import { toast } from "react-toastify";
 import DashboardLayout from "../components/DashboardLayout";
 import CustomerTable from "./components/CustomerTable";
 import StoreAdminData from "./components/StoreAdminTable";
-interface IStoreAdminData {
-  storeAdmins: {
-    withStore: IStoreProps[];
-    withoutStore: IUserProps[];
-  };
-}
+// interface IStoreAdminData {
+//   storeAdmins: {
+//     withStore: IStoreProps[];
+//     withoutStore: IUserProps[];
+//   };
+// }
 export default function AccountPage() {
   const router = useRouter();
   const [users, setUsers] = useState<IUserProps[]>([]);
@@ -26,68 +26,6 @@ export default function AccountPage() {
     withoutStore: [],
   });
   const [stores, setStores] = useState<IStoreProps[]>([]);
-
-  // const getUsersData = async () => {
-  //   try {
-  //     const res = await apiCall.get("/api/user/all");
-  //     const data = res.data.data;
-  //     setUsers(data);
-  //   } catch (error) {
-  //     console.log(error);
-  //     alert("eror");
-  //   }
-  // };
-
-  // const getCustomersData = async () => {
-  //   try {
-  //     const res = await apiCall.get("/api/user/customers");
-  //     const data = res.data.data;
-  //     setCustomers(data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // const getStoreAdmins = async () => {
-  //   try {
-  //     const res = await apiCall.get("/api/store/store-admins");
-  //     const data = res.data.data;
-  //     setStoreAdmins(data);
-  //   } catch (error) {
-  //     console.log(error);
-  //     alert("eror");
-  //   }
-  // };
-
-  // const getStoresData = async () => {
-  //   try {
-  //     const res = await apiCall.get("/api/store/all");
-  //     const data = res.data.data;
-  //     setStores(data);
-  //     // console.log(data);
-  //   } catch (error) {
-  //     console.log(error);
-  //     alert("Eror");
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getUsersData();
-  //   getStoresData();
-  //   getCustomersData();
-  //   getStoreAdmins();
-  // }, []);
-  // cek role, kalau tidak bisa berikan alert
-  // useEffect(() => {
-  //   const jsonData = JSON.parse(localStorage.getItem("user")!);
-  //   if (!jsonData) return;
-  //   if (jsonData.role === "STORE_ADMIN") {
-  //     toast.error("You are not allowed to access this page");
-  //     router.replace("/dashboard/manage-order");
-  //     return;
-  //   }
-  // }, [router]);
-
   useEffect(() => {
     //cara dengan paralel data fetching pake Promise.all
     const fetchAllData = async () => {
@@ -105,7 +43,6 @@ export default function AccountPage() {
         setStoreAdmins(storeAdminsRes.data.data);
       } catch (error: any) {
         if (error.response?.status === 403) {
-          // toast.error("You are not allowed access this page");
           router.replace("/error/forbidden");
         }
         console.log("Failed to fetch data", error);
@@ -118,14 +55,9 @@ export default function AccountPage() {
     <DashboardLayout>
       <div className="flex flex-col gap-4 ">
         <div className="flex flex-col 2xl:flex-row 2xl:justify-between gap-3">
-          <CustomerTable
-            // customers={customers}
-            stores={stores}
-            className="w-full"
-          />
+          <CustomerTable stores={stores} className="w-full" />
           <StoreAdminData storeAdmins={storeAdmins} className="w-full" />
         </div>
-        {/* <StoreData className=" mb-8" stores={stores} /> */}
       </div>
     </DashboardLayout>
   );
